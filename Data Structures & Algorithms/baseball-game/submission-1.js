7c1b5c53d041e572a4db3e49f5ca56753628527c
renaud <52769325+Renaud2002@users.noreply.github.com>
2026-08-18 00:26:03 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        let record = [];
+        let currOp;
+
+        for(let i=0; i<operations.length; i++){
+            currOp = operations[i];
+            if (Number.isInteger(parseInt(currOp, 10))) {
+                record.push(parseInt(currOp, 10));
+                console.log(record);
+            } else if(currOp === "+") {
+                record.push(record[record.length-2] + record[record.length-1]);
+            } else if(currOp === "D") {
+                record.push(record[record.length-1]*2);
+            } else {
+                record.pop();
+            }
+        }
+
+        console.log(record);
+
+        const sum = record.reduce((accumulator, current) => accumulator + current, 0);
+
+        return sum;
+
+
+    }
+}
